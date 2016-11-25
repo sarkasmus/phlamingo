e657333e9c8d5d08a4bb052adbac6d88fb7832ff
Michal Doubek <michal@doubkovi.cz>
2016-11-25 16:04:58 +0100

    Added tests for Request and Response classes

---
+++ phlamingo/tests/http/RequestTest.php
@@ -0,0 +1,133 @@
+<?php
+
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+
+    namespace Phlamingo\Tests\HTTP;
+
+
+    use Phlamingo\HTTP\Request;
+
+    /**
+     * Unit test case of class Phlamingo\HTTP\Request
+     */
+    class RequestTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * Tested class
+         * @var Request
+         */
+        private $Request;
+
+        /**
+         * Instance the tested class
+         */
+        protected function setUp()
+        {
+            $this->Request = new Request(
+                "/article/54",
+                "GET",
+                "1.1",
+                array ("Id" => 54),
+                array(
+                    "Accept-Languages: cs, en;q=0.8, sk",
+                    "Accept-Charset: UTF-8",
+                    "Accept-Encoding: compress, qzip;q=0.5",
+                ),
+                array(),
+                array(),
+                ""
+            );
+        }
+
+        /**
+         * Tests that all Request values are correct
+         */
+        public function testRequestValues()
+        {
+            // Tests that values given within constructor are correct
+            $this->assertEquals("/article/54", $this->Request->URI);
+            $this->assertEquals("GET", $this->Request->Method);
+            $this->assertEquals("1.1", $this->Request->Version);
+            $this->assertEquals(array("Id" => 54), $this->Request->Params);
+
+            // Tests that headers are parsed correctly
+            $this->assertEquals(array(
+                "Accept-Languages" => "cs,en;q=0.8,sk",
+                "Accept-Charset" => "UTF-8",
+                "Accept-Encoding" => "compress,qzip;q=0.5",
+            ), $this->Request->Headers);
+
+            $this->assertEquals(array(), $this->Request->Cookies);
+            $this->assertEquals(array(), $this->Request->Files);
+            $this->assertEquals("", $this->Request->Content);
+
+            // Tests Accept headers are parsed correctly
+            $this->assertEquals(
+                array(
+                    ["language" => "cs"],
+                    ["language" => "en", "q" => "0.8"],
+                    ["language" => "sk"]
+                ),
+                $this->Request->Languages
+            );
+
+            $this->assertEquals(
+                array(
+                    ["charset" => "UTF-8"]
+                ),
+                $this->Request->Charsets
+            );
+
+            $this->assertEquals(array(
+                ["encoding" => "compress"],
+                ["encoding" => "qzip", "q" => "0.5"]
+            ),
+            $this->Request->Encodings
+            );
+        }
+
+        /**
+         * Tests ParseAccept helper
+         */
+        public function testParseAccept()
+        {
+            $this->assertEquals(array(
+                    ["name" => "1"],
+                    ["name" => "2"],
+                    ["name" => "3"]
+                ),
+                $this->Request->ParseAccept("1, 2, 3", "name")
+            );
+
+            $this->assertEquals(array(
+                    ["name" => "1"],
+                    ["name" => "2", "q" => "0.8"],
+                    ["name" => "3"]
+                ),
+                $this->Request->ParseAccept("1, 2;q=0.8, 3", "name")
+            );
+        }
+
+        /**
+         * Tests ParseHeaders helper
+         */
+        public function testParseHeaders()
+        {
+            $this->assertEquals(array(
+                    "Content-Type" => "text/html",
+                    "Accept-Language" => "en"
+                ),
+                // will test removing white chars too
+                $this->Request->ParseHeaders(array("Content-Type: text/html", "Accept-Language: en  "))
+            );
+        }
+    }
---
+++ phlamingo/tests/http/ResponseTest.php
@@ -0,0 +1,89 @@
+<?php
+
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+
+    namespace Phlamingo\Tests\HTTP;
+
+
+    use Phlamingo\HTTP\Exceptions\HttpException;
+    use Phlamingo\HTTP\Response;
+
+    /**
+     * Unit test case of class Phlamingo\HTTP\Response
+     */
+    class ResponseTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * Tested class
+         */
+        private $Response;
+
+        /**
+         * Instance the tested class
+         */
+        public function setUp()
+        {
+            $this->Response = new Response("Hello world", "1.1", 200, "UTF-8", ["Content-Type: text/html"]);
+            $this->Response->Reflection = new \ReflectionClass($this->Response);
+        }
+
+        /**
+         * Tests properties and getters/setters
+         */
+        public function testResponseProperties()
+        {
+            $this->assertEquals("Hello world", $this->Response->Content);
+            $this->assertEquals("1.1", $this->Response->Version);
+            $this->assertEquals(200, $this->Response->StatusCode);
+            $this->assertEquals("OK", $this->Response->Status);
+
+            $this->Response->StatusCode = 500;
+            $this->assertEquals(500, $this->Response->StatusCode);
+            $this->assertEquals("Internal Server Error", $this->Response->Status);
+
+            $this->assertEquals("UTF-8", $this->Response->Charset);
+            $this->assertEquals(["Content-Type: text/html"], $this->Response->Headers);
+
+            $this->Response->AddHeader("Date: Tue, 15 Nov 1994 08:12:31 GMT");
+            $this->assertEquals(["Content-Type: text/html", "Date: Tue, 15 Nov 1994 08:12:31 GMT"], $this->Response->Headers);
+        }
+
+        /**
+         * Tests reactions when trying to input invalid values
+         */
+        public function testExceptions()
+        {
+            try
+            {
+                // setting invalid status code
+                $this->Response->StatusCode = 2156;
+                $this->fail("exception wasn't thrown");
+            }
+            catch (HttpException $e) {}
+
+            try
+            {
+                // setting invalid status code
+                $response = new Response("some content", "1.1", 2156);
+                $this->fail("exception wasn't thrown");
+            }
+            catch (HttpException $e) {}
+
+            try
+            {
+                // setting invalid version
+                $response = new Response("some content", "invalid version");
+                $this->fail("exception wasn't thrown");
+            }
+            catch (HttpException $e) {}
+        }
+    }
