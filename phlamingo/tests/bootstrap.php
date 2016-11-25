57f85421c3aeabdc3244d78a9b57b0c4bc73abc2
Michal Doubek <michal@doubkovi.cz>
2016-11-25 14:57:09 +0100

    Implemented autoloading and config for all tests in phlamingo/tests

---
+++ phlamingo/tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+
+    $loader = require __DIR__ . "/../../vendor/autoload.php";
+    \Phlamingo\Core\Object::SetEnviroment(false);
