6edb6ad9b8f3d128698d889043ce7d83a40a0329
Michal Doubek <michal@doubkovi.cz>
2016-12-29 10:15:26 +0100

    Fixed added missing ContainerSingleton

---
+++ phlamingo/di/ContainerSingleton.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+
+    namespace Phlamingo\Di;
+
+
+    /**
+     * {Description}
+     */
+    final class ContainerSingleton
+    {
+        protected static $Container;
+
+        public static function GetContainer()
+        {
+            if (isset(self::$Container))
+            {
+                return self::$Container;
+            }
+            else
+            {
+                self::$Container = new Container();
+                return self::$Container;
+            }
+        }
+    }
