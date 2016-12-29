9915bfa9748d5dd8c8fc44865e6ba33bbbd02262
Michal Doubek <michal@doubkovi.cz>
2016-12-29 19:52:19 +0100

    Fixed added missing class BaseFactory

---
+++ phlamingo/di/BaseFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+
+    namespace Phlamingo\Di;
+    use Phlamingo\Di\Exceptions\DIContainerException;
+
+
+    /**
+     * {Description}
+     */
+    abstract class BaseFactory
+    {
+        protected $Singleton = null;
+        protected $Container;
+
+        public abstract function Make();
+
+        public function __invoke()
+        {
+            $this->Container = ContainerSingleton::GetContainer();
+
+            if (is_object($this->Make()))
+            {
+                return $this->Make();
+            }
+            else
+            {
+                throw new DIContainerException("Factory " . self::class . " doesn't return object");
+            }
+        }
+    }
