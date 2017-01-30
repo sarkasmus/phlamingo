5e3d8d18a9559a11e6dd26e2f320b56ae16eee84
Michal Doubek <michal@doubkovi.cz>
2017-01-30 14:47:24 +0100

    Added missing CompileException

---
+++ phlamingo/nebula/exceptions/CompileException.php
@@ -0,0 +1,20 @@
+<?php    
+    
+    /**
+     * @author Michal Doubek <michal@doubkovi.cz>
+     * @license LGPL 3
+     *
+     * This code is distributed under LGPL license version 3
+     * For full license information view LICENSE file which is
+     * Distributed with this source code
+     *
+     * This source code is part of Phlamingo project
+     */
+    
+    namespace Phlamingo\Nebula\Exceptions;
+
+    
+    /**
+     * {Description}
+     */
+    class CompileException extends \Exception { }
